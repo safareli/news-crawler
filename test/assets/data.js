5a1a5c45106ab6cd0a51be51fb4a4cfb5200e55c
Irakli Safareli <i.safareli@gmail.com>
2014-07-26 11:27:11 +0000

    update dependencies and implement base getDataByUrl

--- test/assets/data.js
+++ test/assets/data.js
@@ -1,14 +1,15 @@
-var data = {
+/*
+module.exports.each = function(functionName,func){
+    return data[functionName].forEach.bind(data[functionName],function(element, index, array){
+        func.apply(undefined,[element].concat(index))
+    })
+};*/
+module.exports = {
     getUrlsById:[
         //[arguments]
         [84516],//http://www.top.ge/stats.php?ID=84515
     ],
     getDataByUrl:[
-        ['http://news.ge/ge/news/story/98560-zrdiloba-itsode-da-istsavle-rogor-unda-ilaparako-deputatebs-shoris-garchevebi-parlamentshi']
+        [{url:'http://news.ge/ge/news/story/98883-batono-manana-tqvents-dagatsures'}]
     ]
-};
-module.exports.each = function(functionName,func){
-    return data[functionName].forEach.bind(data[functionName],function(element, index, array){
-        func.apply(undefined,element.concat(index))
-    })
 };
--- test/news-crawler_test.js
+++ test/news-crawler_test.js
@@ -9,13 +9,18 @@
 'use strict';
 
 var chai = require('chai'),
+    chaiAsPromised = require("chai-as-promised"),
     crawler = require('../lib/news-crawler.js'),
-    data = require('./assets/data.js');
+    data = require('./assets/data.js'),
+    Q = require('q');
+
+
+chai.use(chaiAsPromised);
 chai.should();
 describe('news-crawler module', function(){
-    describe('#getUrlsById()', function(){
-        it('should return array of urls', data.each('getLinksById',function(args){
-            var result = crawler.getLinksById.apply(crawler,args);
+    /*describe('#getUrlsById()', function(){
+        it('should return array of urls', data.each('getUrlsById',function(args){
+            var result = crawler.getUrlsById.apply(crawler,args);
             result.should.be.array();
             //TODO: es ori xazi rog adaEBAS GAASWOREBS
             result.forEach(function(item){
@@ -23,11 +28,20 @@ describe('news-crawler module', function(){
                 item.should.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})/i);
             });
         }));
-    });
+    });*/
     describe('#getDataByUrl()', function(){
-        it('should return array of urls', data.each('getLinksById',function(args){
-            crawler.getLinksById.apply(crawler,args)
-                .should.have.keys(['image', 'title', 'description', 'url', 'site_name', 'type', 'date']);
-        }));
+        it('should return data object of news by url', function(){
+            //todo date ar modis da davaimplementiro
+            var keys = ['image', 'title', 'description', 'url', 'site_name', 'type', 'date'];
+            return Q.all(data.getDataByUrl.map(function(){
+                return crawler.getDataByUrl.apply(crawler,args);
+            }).reduce(function(promises,promise){
+                return promises.concat([
+                    promise.should.be.fulfilled,
+                    promise.should.eventually.have.keys(keys)
+                ]);
+            },[]));
+
+        });
     });
 });
