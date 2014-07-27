efae366b469166d1f9564d5595cdc8e3887d68d2
Irakli Safareli <i.safareli@gmail.com>
2014-07-27 14:59:10 +0000

    change function names and make more flexible

--- test/assets/data.js
+++ test/assets/data.js
@@ -1,8 +1,23 @@
+var moment = require('moment');
+moment.lang('ka');
+
 module.exports = {
-    getUrlsById:[
+    getUrls:[
         [84516]
     ],
-    getDataByUrl:[
-        [{url:'http://news.ge/ge/news/story/98883-batono-manana-tqvents-dagatsures'}]
+    getData:[
+        [{
+            url:'http://news.ge/ge/news/story/98883-batono-manana-tqvents-dagatsures',
+            keys: ['image', 'title', 'description', 'url', 'site_name', 'category','date'],
+            source: [ 'meta[property="og:{key}"]', 'attr', 'content'],
+            extras: {
+                date: function($,window){
+                    return moment($('.info-date').text(), "DD MMMM HH:mm").lang('ka').unix();
+                },
+                category: function($,window){
+                    return $('.cufonGeo').text();
+                }
+            }
+        }]
     ]
 };
--- test/news-crawler_test.js
+++ test/news-crawler_test.js
@@ -10,19 +10,18 @@
 
 var chai = require('chai'),
     chaiAsPromised = require("chai-as-promised"),
-    crawler = require('../lib/news-crawler.js'),
-    data = require('./assets/data.js'),
+    crawler = require('../lib/news-crawler'),
+    data = require('./assets/data'),
     Q = require('q');
 
 
 chai.use(chaiAsPromised);
 chai.should();
 describe('news-crawler module', function(){
-    describe('#getUrlsById()', function(){
+    describe('#getUrls()', function(){
         it('should return array of urls', function(){
-            return Q.all(data.getUrlsById.map(function(args,i){
-                return crawler.getUrlsById.apply(crawler,args);
-            }).reduce(function(promises,promise){
+            return Q.all(data.getUrls.reduce(function(promises,args){
+                var promise = crawler.getUrls.apply(crawler,args);
                 return promises.concat([
                     promise.should.be.fulfilled,
                     promise.should.eventually.be.instanceof(Array),
@@ -36,17 +35,15 @@ describe('news-crawler module', function(){
             },[]));
         });
     });
-    describe('#getDataByUrl()', function(){
+    describe('#getData()', function(){
         it('should return data object of news by url', function(){
-            var keys = ['image', 'title', 'description', 'url', 'site_name', 'type'];
-            return Q.all(data.getDataByUrl.map(function(args){
-                return crawler.getDataByUrl.apply(crawler,args);
-            }).reduce(function(promises,promise){
+            return Q.all(data.getData.reduce(function(promises,args){
+                var promise = crawler.getData.apply(crawler,args);
                 return promises.concat([
                     promise.should.be.fulfilled,
-                    promise.should.eventually.have.keys(keys)
+                    promise.should.eventually.have.keys(args[0].keys)
                 ]);
-            },[]));
+            }, []));
         });
     });
 });
